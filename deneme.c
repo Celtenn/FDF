1fd4f48a1dded812afe6d307584792dd85361ddd
Celtenn <enescelten@gmail.com>
2025-01-25 06:30:19 +0300

    Add files via upload

--- deneme.c
+++ deneme.c
@@ -3,12 +3,190 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define MAX_ROWS 1000   // Maksimum satır sayısı
-#define TILE_SIZE 20    // Her bir hücrenin piksel boyutu
+#define OFFSET_X 500
+#define OFFSET_Y 100
+#define SCALE_FACTOR 0.5
+#define BUFFER_SIZE 3
+#define MAX_ROWS 100000   // Maksimum satır sayısı
+#define TILE_SIZE 10    // Her bir hücrenin piksel boyutu
  #include <string.h>
 #include <stdlib.h>
 #include "mlx.h"
 #include <math.h>
+
+int	ft_atoi(char *str)
+{
+	unsigned int		i;
+	int					sign;
+	unsigned long int	number;
+
+	i = 0;
+	number = 0;
+	while (str[i])
+	{
+        if (str[i] < '0' || str[i] > '9')
+        {
+            return (-10);
+        }
+		number = (str[i] - '0') + (number * 10);
+		i++;
+	}
+	return (number);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == c)
+			return (str);
+		str++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *str, char *nbr)
+{
+	size_t	p;
+	size_t	i;
+	char	*arr;
+
+	if (!str)
+	{
+		str = (char *)malloc(sizeof(char) * 1);
+		str[0] = '\0';
+	}
+	if (!str || !nbr)
+		return (NULL);
+	arr = malloc(ft_strlen(str) + ft_strlen(nbr) + 1);
+	if (!arr)
+		return (NULL);
+	i = 0;
+	p = 0;
+	while (str[p] != '\0')
+		arr[i++] = str[p++];
+	i = 0;
+	while (nbr[i] != '\0')
+		arr[p++] = nbr[i++];
+	arr[p] = '\0';
+	free(str);
+	return (arr);
+}
+
+char	*ft_line(char *str)
+{
+	char	*arr;
+	int		i;
+
+	i = 0;
+	if (!str[i])
+		return (0);
+	while (str[i] && str[i] != '\n')
+		i++;
+	arr = malloc(i + 2);
+	if (!arr)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
+
+char	*ft_last(char *str)
+{
+	char	*arr;
+	int		i;
+	int		p;
+
+	i = 0;
+	p = 0;
+	while (str[p] && str[p] != '\n')
+	{
+		p++;
+	}
+	if (!str[p])
+	{
+		free(str);
+		return (0);
+	}
+	arr = malloc(ft_strlen(str) - p + 1);
+	if (!arr)
+		return (0);
+	p++;
+	while (str[p])
+		arr[i++] = str[p++];
+	arr[i] = '\0';
+	free(str);
+	return (arr);
+}
+
+char	*ft_read(char *str, int fd)
+{
+	int		read_len;
+	char	*buffer;
+
+	read_len = 1;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	while (!ft_strchr(str, '\n') && read_len != 0)
+	{
+		read_len = read(fd, buffer, BUFFER_SIZE);
+		if (read_len == -1)
+		{
+			free(buffer);
+			return (0);
+		}
+		buffer[read_len] = '\0';
+		str = ft_strjoin(str, buffer);
+	}
+	free(buffer);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*arr;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(str);
+		str = NULL;
+		return (0);
+	}
+	str = ft_read(str, fd);
+	if (!str)
+		return (0);
+	arr = ft_line(str);
+	str = ft_last(str);
+	return (arr);
+}
 
 int	countw(char const *s, char c)
 {
@@ -120,7 +298,7 @@ int count_values(char **values)
     return count;
 }
 
-int **read_fdf_file(char *filename, int *rows, int *cols) 
+unsigned int **read_fdf_file(char *filename, int *rows, int *cols) 
 {
     int fd = open(filename, O_RDONLY);
     if (fd < 0) 
@@ -131,13 +309,12 @@ int **read_fdf_file(char *filename, int *rows, int *cols)
 
     char *line = NULL;
     char **values;
-    size_t len = 0;
-    ssize_t read;
-    int **map = NULL;
+    unsigned int **map = NULL;
     int col = 0;
-
     int row = 0;
-    while ((read = getline(&line, &len, fdopen(fd, "r"))) != -1) 
+    int temp = 0;
+
+    while ((line = get_next_line(fd))) 
     {
         values = ft_split(line, ' ');
         if (!map) 
@@ -146,10 +323,20 @@ int **read_fdf_file(char *filename, int *rows, int *cols)
             map = malloc(sizeof(int *) * MAX_ROWS);
         }
         map[row] = malloc(sizeof(int) * (*cols));
+        col = 0;
         while (col < *cols) 
         {
-            map[row][col] = atoi(values[col]);
-            col++;
+            if (ft_atoi(values[col]) == -10)
+            {
+                unsigned int hex_value = strtol(values[col], NULL, 16);
+                map[row][col] = hex_value;
+                col++;
+            }
+            else
+            {
+                map[row][col] = ft_atoi(values[col]);
+                col++;
+            }
         }
         row++;
         free(values);
@@ -164,8 +351,8 @@ void iso_projection(int *x, int *y, int z)
     int prev_x = *x;
     int prev_y = *y;
 
-    *x = (prev_x - prev_y) * cos(0.523599); // 30 derece
-    *y = (prev_x + prev_y) * sin(0.523599) - z; 
+    *x = (prev_x - prev_y) * cos(0.523599) * SCALE_FACTOR + OFFSET_X; // 30 derece
+    *y = (prev_x + prev_y) * sin(0.523599) * SCALE_FACTOR + OFFSET_Y - z;
 }
 
 void draw_line(int x0, int y0, int x1, int y1, void *mlx, void *win) 
@@ -204,7 +391,7 @@ void draw_line(int x0, int y0, int x1, int y1, void *mlx, void *win)
     }
 }
 
-void draw_map(int **map, int rows, int cols, void *mlx, void *win) 
+void draw_map(unsigned int **map, int rows, int cols, void *mlx, void *win) 
 {
     int y = 0;
     int x = 0;
@@ -212,15 +399,28 @@ void draw_map(int **map, int rows, int cols, void *mlx, void *win)
     int y_proj = 0;
     int x_next = 0;
     int y_next = 0;
+    int max_z = -1; // En yüksek z değeri
+    // Tepe noktasının belirlenmesi
+    int peak_x = 0, peak_y = 0;
 
     while (y < rows) 
     {
+        x = 0;
         while (x < cols) 
         {
+            // Her nokta için projeksiyon hesapla
             x_proj = x * TILE_SIZE;
             y_proj = y * TILE_SIZE;
             iso_projection(&x_proj, &y_proj, map[y][x]);
 
+            // Eğer bu nokta en yüksek z noktasına sahipse, bunu kaydet
+            if (map[y][x] > max_z) {
+                max_z = map[y][x];
+                peak_x = x_proj;
+                peak_y = y_proj;
+            }
+
+            // Diğer çizimleri yap (yan ve alt çizgileri)
             if (x < cols - 1) 
             {
                 x_next = (x + 1) * TILE_SIZE;
@@ -245,7 +445,7 @@ int main(int argc, char **argv)
 {
     int rows;
     int cols;
-    int **map;
+    unsigned int **map;
     void *mlx;
     void *win;
 
@@ -256,7 +456,7 @@ int main(int argc, char **argv)
     }
 
     mlx = mlx_init();
-    win = mlx_new_window(mlx, 1400, 800, "FDF");
+    win = mlx_new_window(mlx, 900, 600, "FDF");
 
     
     map = read_fdf_file(argv[1], &rows, &cols);

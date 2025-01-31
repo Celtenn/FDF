d78091f408a0f0a936a5b265ff4595a3cde39032
Celtenn <enescelten@gmail.com>
2025-01-31 11:09:24 +0300

    Add files via upload

--- deneme.c
+++ deneme.c
@@ -7,7 +7,7 @@ unsigned int atoi_hex(char *str)
 
     while (*str)
     {
-        if (*str <= 9 && *str >= 0)
+        if (*str >= '0' && *str <= '9')
             val = *str - '0';
         else if (*str >= 'a' && *str <= 'f')
             val = *str - 'a' + 10;
@@ -22,6 +22,7 @@ unsigned int atoi_hex(char *str)
 unsigned int **read_fdf_file(char *filename, t_data *data)
 {
 	r_data r_data;
+	char *base = "0xADD8E6";
     int fd = open(filename, O_RDONLY);
     if (fd < 0) 
     {
@@ -46,24 +47,21 @@ unsigned int **read_fdf_file(char *filename, t_data *data)
         {
             data->cols = count_values(r_data.values);
             r_data.map = malloc(sizeof(unsigned int *) * (data->cols + 1));
-			data->color = malloc(sizeof(unsigned int *) * (data->cols + 1));
+            data->color = malloc(sizeof(unsigned int *) * (data->cols + 1));
         }
-        r_data.map[r_data.row] = malloc(sizeof(unsigned int) * (data->cols));
-		data->color[r_data.row] = malloc(sizeof(unsigned int) * (data->cols));
+        r_data.map[r_data.row] = malloc(sizeof(unsigned int) * (data->cols + 1));
+        data->color[r_data.row] = malloc(sizeof(unsigned int) * (data->cols + 1));
         r_data.col = 0;
         while (r_data.col < data->cols) 
         {
-            if (!atoi(r_data.values[r_data.col]))
+            if (ft_atoi(r_data.values[r_data.col]) == -10)
             {
 				r_data.j = 0;
                 r_data.temp = ft_split(r_data.values[r_data.col], ',');
-                r_data.tmp = ft_atoi(r_data.temp[r_data.j]);
+                r_data.tmp = atoi(r_data.temp[r_data.j]);
                 r_data.map[r_data.row][r_data.col] = r_data.tmp;
-				data->color[r_data.row][r_data.col] = 0x00FFFFFF; // Varsayılan renk (beyaz)
-				if (r_data.temp[r_data.j + 1] != NULL) // Renk bilgisi varsa
-        		{
-            		data->color[r_data.row][r_data.col] = atoi_hex(r_data.temp[r_data.j + 1]); // Renk bilgisini hex'ten al
-        		}
+				r_data.tmp = atoi_hex(r_data.temp[r_data.j + 1]);
+                data->color[r_data.row][r_data.col] = r_data.tmp;
                 r_data.col++;
 				while (r_data.temp[r_data.j])
 				{
@@ -74,8 +72,8 @@ unsigned int **read_fdf_file(char *filename, t_data *data)
             }
             else
             {
-                r_data.map[r_data.row][r_data.col] = ft_atoi(r_data.values[r_data.col]);
-				data->color[r_data.row][r_data.col] = 0x00FFFFFF; // Varsayılan renk (beyaz)
+                r_data.map[r_data.row][r_data.col] = atoi(r_data.values[r_data.col]);
+				data->color[r_data.row][r_data.col] = atoi_hex(base);
                 free(r_data.values[r_data.col]);
                 r_data.col++;
             }
@@ -91,25 +89,19 @@ unsigned int **read_fdf_file(char *filename, t_data *data)
 
 void iso_projection(int *x, int *y, int z, t_data *data) 
 {
-	int ex = 3;
-	float scale = 1;
-	float scale_x = 1800.0 / (float)(data->cols);
-	float scale_y = 900.0 / (data->rows);
-	if (scale_x < scale_y)
-		scale = scale_x;
-	if (scale_x > scale_y)
-		scale = scale_y;
-	if (scale < 0.3)
-		scale = 0.20;
-	if (scale > 1.0)
-		scale = 0.8;
-	//int offsetx = 1800 / 2 - ((data->cols * TILE_SIZE) * scale) / 2;
-	//int offsety = 900 / 2 - ((data->rows * TILE_SIZE) * scale) / 2;
+	(void)data;
+	int ex = 1;
+	//float scale = 1;
+	//float scale_x = 1800 / (float)(data->cols * TILE_SIZE);
+	//float scale_y = 900 / (float)(data->rows * TILE_SIZE);
+	
+	//int offsetx = 1800 / 2 - ((data->cols * TILE_SIZE) * scale);
+	//int offsety = 900 / 2 - ((data->rows * TILE_SIZE) * scale);
     int prev_x = *x;
     int prev_y = *y;
 
     *x = (prev_x - prev_y) * cos(0.6) * 0.2 + 600; // 150 derece
-    *y = (prev_x + prev_y) * sin(0.6) * 0.2 + 400 - (z * ex);
+    *y = (prev_x + prev_y) * sin(0.6) * 0.2 + 200 - (z * ex);
 }
 
 int is_inside(int x, int y) 
@@ -124,12 +116,12 @@ int clip_line(int *x0, int *y0, int *x1, int *y1)
     }
 
     if (*x0 < 0) *x0 = 0;
-    if (*x0 >= 1800) *x0 = 1600 - 1;
+    if (*x0 >= 1600) *x0 = 1600 - 1;
     if (*y0 < 0) *y0 = 0;
     if (*y0 >= 900) *y0 = 900 - 1;
 
     if (*x1 < 0) *x1 = 0;
-    if (*x1 >= 1800) *x1 = 1600 - 1;
+    if (*x1 >= 1600) *x1 = 1600 - 1;
     if (*y1 < 0) *y1 = 0;
     if (*y1 >= 900) *y1 = 900 - 1;
 
@@ -138,8 +130,8 @@ int clip_line(int *x0, int *y0, int *x1, int *y1)
 
 void draw_line(int x0, int y0, int x1, int y1, t_data *data, unsigned int color) 
 {
-	 //if (!clip_line(&x0, &y0, &x1, &y1))
-        //return;
+	 if (!clip_line(&x0, &y0, &x1, &y1))
+        return;
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
     int sx;
@@ -201,14 +193,14 @@ void draw_map(unsigned int **map, t_data *data)
                 x_next = (x + 1) * TILE_SIZE;
                 y_next = y * TILE_SIZE;
                 iso_projection(&x_next, &y_next, map[y][x + 1], data);
-                draw_line(x_proj, y_proj, x_next, y_next, data, data->color[y][x + 1]);
+                draw_line(x_proj, y_proj, x_next, y_next, data, data->color[y][x]);
             }
             if (y < data->rows - 1) 
             {
                 x_next = x * TILE_SIZE;
                 y_next = (y + 1) * TILE_SIZE;
                 iso_projection(&x_next, &y_next, map[y + 1][x], data);
-                draw_line(x_proj, y_proj, x_next, y_next, data, data->color[y + 1][x]);
+                draw_line(x_proj, y_proj, x_next, y_next, data, data->color[y][x]);
             }
             x++;
         }
@@ -270,16 +262,6 @@ int key_hook_esc(int keycode, void *param)
         	}
         	free(data->map);
     	}
-		if (data->color != NULL) 
-		{
-			i = 0;
-        	while (data->color[i] != NULL) 
-			{
-            	free(data->color[i]);
-				i++;
-        	}
-        	free(data->color);
-    	}
 		if (data->image != NULL)
 			mlx_destroy_image(data->mlx, data->image);
     	if (data->win != NULL) 
@@ -316,11 +298,11 @@ int main(int argc, char **argv)
     data.win = NULL;
     data.image = NULL;
     data.narr = NULL;
-	data.len = (1920 * 32) / 8;
+	data.len = (1800 * 32) / 8;
 	data.bitt = 32;
     data.mlx = mlx_init();
-    data.win = mlx_new_window(data.mlx, 1920, 1080, "fdf");
-	data.image = mlx_new_image(data.mlx, 1920, 1080);
+    data.win = mlx_new_window(data.mlx, 1800, 900, "fdf");
+	data.image = mlx_new_image(data.mlx, 1800, 900);
 	data.narr = mlx_get_data_addr(data.image, &data.bitt, &data.len, &data.endian);
 
     data.map = read_fdf_file(argv[1], &data);
